4f9ef7747a6f6186cb5e2b433b0c0abf6f516195
Adil Tanbir <aadiltanbir@gmail.com>
2026-04-29 17:33:01 +0530

    Update SortOption.kt

--- app/src/main/java/com/smartpdfhub/data/model/SortOption.kt
+++ app/src/main/java/com/smartpdfhub/data/model/SortOption.kt
@@ -1,5 +1,16 @@
 package com.smartpdfhub.data.model
 
+// BUG FIX #3: Added NAME_ASC, NAME_DESC, SIZE_ASC, SIZE_DESC, DATE_ASC, DATE_DESC
+// which are referenced in MainActivity and PDFViewModel but were missing from the enum
 enum class SortOption {
-    NAME, DATE, SIZE, RECENT
+    RECENT,
+    NAME,
+    NAME_ASC,
+    NAME_DESC,
+    DATE,
+    DATE_ASC,
+    DATE_DESC,
+    SIZE,
+    SIZE_ASC,
+    SIZE_DESC
 }
